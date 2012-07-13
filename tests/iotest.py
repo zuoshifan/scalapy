b490abfa50fc65d042535f52931084e6ef8d97c6
Kiyoshi Masui <kiyo@tpb120.sunnyvale>
2012-07-13 12:23:55 -0400

    Wrote and tested chunked MPIIO reads for large matricies.

--- tests/iotest.py
+++ tests/iotest.py
@@ -19,7 +19,7 @@ rank = comm.Get_rank()
 #comm = Comm()
 
 import pyscalapack.core as pscore
-from pyscalapack import npyutils  
+from pyscalapack import npyutils, blockcyclic
 #import npyutils as npyutils
 
 # Number of processors per side on grid
@@ -82,9 +82,9 @@ class TestIO(unittest.TestCase):
         comm.barrier()
 
     def test_read_fortran(self):
-        self.mat = np.asfortranarray(self.mat)
+        fmat = np.asfortranarray(self.mat)
         if rank == 0:
-            np.save("tmp_test_origional.npy", self.mat)
+            np.save("tmp_test_origional.npy", fmat)
         comm.barrier()
 
         Amat = pscore.DistributedMatrix.from_npy("tmp_test_origional.npy",
@@ -94,9 +94,9 @@ class TestIO(unittest.TestCase):
         self.assertTrue(pscore.matrix_equal(Amat, Bmat))
 
     def test_write_fortran(self):
-        self.mat = np.asfortranarray(self.mat)
+        fmat = np.asfortranarray(self.mat)
         
-        Dmat = pscore.DistributedMatrix.fromarray(self.mat,
+        Dmat = pscore.DistributedMatrix.fromarray(fmat,
                                                   blocksize=(16, 16))
         Dmat.to_npy("tmp_test_origional.npy")
         if rank == 0:
@@ -105,9 +105,9 @@ class TestIO(unittest.TestCase):
             self.assertTrue(np.allclose(Bmat, self.mat))
 
     def test_read_C(self):
-        self.mat = np.ascontiguousarray(self.mat)
+        cmat = np.ascontiguousarray(self.mat)
         if rank == 0:
-            np.save("tmp_test_origional.npy", self.mat)
+            np.save("tmp_test_origional.npy", cmat)
         comm.barrier()
 
         Amat = pscore.DistributedMatrix.from_npy("tmp_test_origional.npy",
@@ -117,9 +117,9 @@ class TestIO(unittest.TestCase):
         self.assertTrue(pscore.matrix_equal(Amat, Bmat))
 
     def test_write_C(self):
-        self.mat = np.ascontiguousarray(self.mat)
+        cmat = np.ascontiguousarray(self.mat)
         
-        Dmat = pscore.DistributedMatrix.fromarray(self.mat,
+        Dmat = pscore.DistributedMatrix.fromarray(cmat,
                                                   blocksize=(16, 16))
         Dmat.to_npy("tmp_test_origional.npy", fortran_order=False)
         if rank == 0:
@@ -133,6 +133,88 @@ class TestIO(unittest.TestCase):
             for f in files:
                 os.remove(f)
         comm.barrier()
+
+
+class TestLargeFile(unittest.TestCase):
+    """These tests make sure reading large files where each node uses more than
+    2Gb of memory works."""
+    
+    def setUp(self):
+        self.fname = '/dev/zero'
+        # choose n such that each process has more than 2G of data.
+        n = npx * 18000
+        self.shape = (n, n)
+        self.blocksize = [512, 512]
+        self.m = pscore.DistributedMatrix(self.shape, blocksize=self.blocksize,
+                                          dtype=np.dtype(float))
+        self.m.local_array[...] = 5.
+
+    def test_large_C(self):
+        blockcyclic.mpi_readmatrix(self.fname, MPI.COMM_WORLD, self.shape, 
+                np.dtype(float).type, self.blocksize, 
+                (self.m.context.num_rows, self.m.context.num_cols),
+                order='C', displacement=0, local_array=self.m.local_array)
+        self.assertTrue(np.all(self.m.local_array == 0))
+
+    def test_large_F(self):
+        blockcyclic.mpi_readmatrix(self.fname, MPI.COMM_WORLD, self.shape, 
+                np.dtype(float).type, self.blocksize, 
+                (self.m.context.num_rows, self.m.context.num_cols),
+                order='F', displacement=0, local_array=self.m.local_array)
+        self.assertTrue(np.all(self.m.local_array == 0))
+
+
+class TestChunkedRectangular(unittest.TestCase):
+
+    def setUp(self):
+        self.shape = (432, 653)
+        self.mat = (np.arange(self.shape[0], dtype=np.float64)[:,None]
+                    + 5 * np.arange(self.shape[1], dtype=np.float64)[None,:]
+                    + 10)
+        self.blocksize = [7, 5]
+        self.m = pscore.DistributedMatrix(self.shape, blocksize=self.blocksize,
+                                          dtype=np.dtype(np.float64))
+        self.m.local_array[...] = 5.
+    
+    def test_C(self):
+        cmat = np.ascontiguousarray(self.mat)
+        if rank == 0:
+            cmat.tofile("tmp_test_origional.npy")
+        comm.barrier()
+
+        ref_mat = pscore.DistributedMatrix.fromarray(self.mat,
+                                                  blocksize=self.blocksize)
+        blockcyclic.mpi_readmatrix("tmp_test_origional.npy", MPI.COMM_WORLD,
+                self.shape, np.dtype(float).type, self.blocksize, 
+                (self.m.context.num_rows, self.m.context.num_cols),
+                order='C', displacement=0, local_array=self.m.local_array,
+                max_single_read_size=600000)
+        self.assertTrue(pscore.matrix_equal(self.m, ref_mat))
+        self.assertTrue(np.allclose(self.m.local_array, ref_mat.local_array))
+    
+    def test_F(self):
+        cmat = np.ascontiguousarray(self.mat)
+        if rank == 0:
+            cmat.T.tofile("tmp_test_origional.npy")
+        comm.barrier()
+
+        ref_mat = pscore.DistributedMatrix.fromarray(self.mat,
+                                                  blocksize=self.blocksize)
+        blockcyclic.mpi_readmatrix("tmp_test_origional.npy", MPI.COMM_WORLD,
+                self.shape, np.dtype(float).type, self.blocksize, 
+                (self.m.context.num_rows, self.m.context.num_cols),
+                order='F', displacement=0, local_array=self.m.local_array,
+                max_single_read_size=600000)
+        self.assertTrue(pscore.matrix_equal(self.m, ref_mat))
+        self.assertTrue(np.allclose(self.m.local_array, ref_mat.local_array))
+
+    def tearDown(self):
+        if rank == 0:
+            files = glob.glob("tmp_test_*")
+            for f in files:
+                os.remove(f)
+        comm.barrier()
+
 
 
 if __name__ == '__main__' :
